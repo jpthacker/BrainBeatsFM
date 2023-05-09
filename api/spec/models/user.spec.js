e0819719997c71a946e4b9868a21eaeca82d91e9
Jack Thacker <jackthacker89@gmail.com>
2023-05-09 15:15:10 +0100

    Test drive password in User schema

--- api/spec/models/user.spec.js
+++ api/spec/models/user.spec.js
@@ -5,7 +5,50 @@ describe("User model", () => {
   it("it has an email address", () => {
     const user = new User({
       email: "someemail@anything.com",
+      password: "swordfish",
     });
     expect(user.email).toEqual("someemail@anything.com");
   });
+
+  it("it has a password", () => {
+    const user = new User({
+      email: "someemail@anything.com",
+      password: "swordfish",
+    });
+    expect(user.password).toEqual("swordfish");
+  });
+
+  // HAving trouble getting these tests to work without callbacks
+  // (new version of Mongoose doesn't support them)
+
+  // it("can list all users", () => {
+  //   const results = async () => {
+  //     const results = await User.find({}).exec();
+  //     results.then((users) => {
+  //       return users;
+  //     });
+  //   };
+  //   expect(results()).toEqual({});
+  // });
+
+  // it("can save a user", () => {
+  //   const user = new User({
+  //     email: "someone@example.com",
+  //     password: "password",
+  //   });
+
+  //   user.save((err) => {
+  //     expect(err).toBeNull();
+
+  //     User.find((err, users) => {
+  //       expect(err).toBeNull();
+
+  //       expect(users[0]).toMatchObject({
+  //         email: "someone@example.com",
+  //         password: "password",
+  //       });
+  //       done();
+  //     });
+  //   });
+  // });
 });
