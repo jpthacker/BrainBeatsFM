3b478af6ac98b9d5cb46075484dc154921a9b2f5
Jack Thacker <jackthacker89@gmail.com>
2023-05-12 15:57:32 +0100

    Add individual room route to MainRoom

--- frontend/src/components/MainRoom/MainRoom.tsx
+++ frontend/src/components/MainRoom/MainRoom.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const MainRoom = () => {
-  const [room, setRoom] = useState([]);
+  const router = useRouter();
+  const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
     const fetchRooms = async () => {
@@ -13,23 +15,31 @@ const MainRoom = () => {
         },
       });
       let data = await response.json();
-      setRoom(data.room);
-      console.log(data.room)
+      setRooms(data.room);
+      console.log(data.room);
     };
     fetchRooms();
   }, []);
 
+  const handleOpenRoom = (_roomId: any) => {
+    router.push(`/rooms/${_roomId}`);
+  };
+
   return (
-    <div className="grid grid-cols-3 gap-4 place-items-stretch h-56 p-24" >
-      {room.map((r) => (
-        <div className="flex min-w-screen flex-col items-center justify-center gap-12 p-24 rounded-3xl bg-gray-300 dark:bg-slate-800" key={r['_id']}>
-          <h2>{r['name']}</h2>
-          <p>{r['description']}</p>
+    <div className="grid grid-cols-3 gap-4 place-items-stretch h-56 p-24">
+      {rooms.map((r) => (
+        <div
+          className="flex min-w-screen flex-col items-center justify-center gap-12 p-24 rounded-3xl bg-gray-300 dark:bg-slate-800"
+          key={r["_id"]}
+          onClick={() => {
+            handleOpenRoom(`${r["name"]}`);
+          }}>
+          <h2>{r["name"]}</h2>
+          <p>{r["description"]}</p>
         </div>
       ))}
     </div>
-  )
-  
+  );
 };
 
 export default MainRoom;
