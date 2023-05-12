f144d60e02ffeafa1e3ad2aa191cce0191b363d7
Jack Thacker <jackthacker89@gmail.com>
2023-05-12 20:12:56 +0100

    Add dark/light responsive logo to nav

--- frontend/src/app/components/Navbar/Navbar.tsx
+++ frontend/src/app/components/Navbar/Navbar.tsx
@@ -2,7 +2,8 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import logo from "../../../../public/brainbeats-logo-dark-md.png";
+import logoDark from "../../../../public/brainbeats-logo-dark-md.png";
+import logoLight from "../../../../public/brainbeats-logo-light-md.png";
 
 const Navbar = () => {
   const router = useRouter();
@@ -14,7 +15,10 @@ const Navbar = () => {
 
   return (
     <div className="flex flex-row items-center justify-between w-full h-24 px-12">
-      <Image src={logo} width={300} height={20} alt="brainbeatsfm logo" />
+      <picture className="flex flex-col items-center justify-center h-full w-64">
+        <source srcSet={logoDark.src} media="(prefers-color-scheme: dark)" />
+        <Image src={logoLight} alt="My image" width={300} height={300} />
+      </picture>
       <button
         className="py-2 px-4 hover:pointer-cursor"
         onClick={removeUserDetails}>
