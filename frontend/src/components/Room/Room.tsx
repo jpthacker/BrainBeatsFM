f712c349a17d6bffd073481b2b5298462e431f3c
Jack Thacker <jackthacker89@gmail.com>
2023-05-15 16:49:34 +0100

    Add tracks fetch request on render in Room

--- frontend/src/components/Room/Room.tsx
+++ frontend/src/components/Room/Room.tsx
@@ -11,6 +11,7 @@ const Room = () => {
     data: Room | { name: "room" };
     loading: boolean;
   }>({ data: { name: "room" }, loading: true });
+  const [tracks, setTracks] = React.useState([]);
 
   React.useEffect(() => {
     const localRoomName: string = window.localStorage.getItem("roomName")!;
@@ -28,7 +29,20 @@ const Room = () => {
       setRoom({ data: data.room, loading: false });
     };
 
+    const fetchTracks = async () => {
+      let response = await fetch(`/api/tracks/${localRoomName}`, {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+      });
+      let data = await response.json();
+      setTracks(data.tracks);
+    };
+
     fetchRoom();
+    fetchTracks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -36,6 +50,15 @@ const Room = () => {
     <div className="min-w-screen min-h-screen p-16">
       <h2>{room.data.name}</h2>
       <h3>{`A room dedicated to AI ${room.data.name} music`}</h3>
+      <div className="">
+        {tracks.map((t) => (
+          <div className="border-2 border-green-600" key={t["_id"]}>
+            <h2>{t["title"]}</h2>
+            <p>{t["owner"]}</p>
+            <p>{t["description"]}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
