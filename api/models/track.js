e64a15252055ebb2413828b5c1abb0492a85a2e9
Jack Thacker <jackthacker89@gmail.com>
2023-05-15 16:35:53 +0100

    Add desc to Track mongoose schema

--- api/models/track.js
+++ api/models/track.js
@@ -4,6 +4,7 @@ const TrackSchema = new mongoose.Schema({
   title: { type: String, required: true },
   owner: { type: String, required: true },
   genre: { type: String, required: true },
+  description: { type: String, required: true },
   url: { type: String, required: true },
 });
 
