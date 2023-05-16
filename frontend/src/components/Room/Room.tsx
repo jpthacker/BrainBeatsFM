6f4a4b939f8e9922cf6af2d871d40a7cfe83ad2b
Jack Thacker <jackthacker89@gmail.com>
2023-05-16 14:18:04 +0100

    Get wavesurfer to work on render and play/pause
    Co-authored-by: Noelia Perez Marin <NoeliaMarin@users.noreply.github.com>

--- frontend/src/components/Room/Room.tsx
+++ frontend/src/components/Room/Room.tsx
@@ -13,7 +13,9 @@ const Room = () => {
     loading: boolean;
   }>({ data: { name: "room" }, loading: true });
   const [tracks, setTracks] = React.useState([]);
-  const [playing, setPlaying] = React.useState(false);
+  const [cards, setCards] = React.useState(false);
+  const [audio, setAudio] = React.useState(false);
+  const waveSurferRef = React.useRef<any | null>(null);
 
   React.useEffect(() => {
     const localRoomName: string = window.localStorage.getItem("roomName")!;
@@ -51,47 +53,71 @@ const Room = () => {
   }, []);
 
   useEffect(() => {
-    if (playing) {
-      var wavesurfer = WaveSurfer.create({
+    if (audio) {
+      const wavesurfer = WaveSurfer.create({
         container: "#waveform",
       });
       wavesurfer.load(tracks[0]["url"]);
       wavesurfer.on("ready", function () {
+        waveSurferRef.current = wavesurfer;
         wavesurfer.play();
       });
+      return () => {
+        wavesurfer.destroy();
+      };
     }
-  }, [playing]);
+  }, [audio]);
+
+  const handleLoadCards = () => {
+    if (cards) {
+      return (
+        <div className="w-full">
+          {tracks.map((t) => (
+            <div
+              className="w-full flex-1 flex-col items-start justify-center p-6 rounded-xl bg-gray-300 dark:bg-[#27273F]"
+              key={t["_id"]}>
+              <div className="flex flex-row items-center justify-between w-full">
+                <button
+                  className="hover:cursor-pointer"
+                  onClick={() => {
+                    waveSurferRef.current.playPause();
+                  }}>
+                  Play/Pause
+                </button>
+                <div className="flex flex-col items-start w-6/12 border-2 border-red-600">
+                  <h2 className="mb-1">{t["title"]}</h2>
+                  <p className="mb-4">{t["owner"]}</p>
+                </div>
+                <div id="waveform" className="w-full text-white"></div>
+              </div>
+              <div className="bg-gradient-to-r from-orange-600 to-pink-400 pt-1">
+                <p className="bg-gray-300 dark:bg-[#27273F] text-center py-3">
+                  {t["description"]}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <button
+          onClick={() => {
+            setCards(true);
+            setAudio(true);
+          }}>
+          Start Listening
+        </button>
+      );
+    }
+  };
 
   return (
     <div className="flex flex-col min-w-screen min-h-screen p-16">
       <div className="min-w-full flex flex-col items-start justify-around gap-4 py-12">
         <h2 className="capitalize">{room.data.name}</h2>
         <h3>{`A room dedicated to AI ${room.data.name} music`}</h3>
-      </div>
-      <div className="">
-        {tracks.map((t) => (
-          <div
-            className="flex flex-col items-start justify-center p-6 rounded-xl bg-gray-300 dark:bg-[#27273F]"
-            key={t["_id"]}>
-            <h2
-              className="mb-1 hover:cursor-pointer"
-              onClick={() => {
-                setPlaying(true);
-              }}>
-              {t["title"]}
-            </h2>
-            <p className="mb-4">{t["owner"]}</p>
-            <div className="bg-gradient-to-r from-orange-600 to-pink-400 pt-1">
-              <p className="bg-gray-300 dark:bg-[#27273F] text-center py-3">
-                {t["description"]}
-              </p>
-            </div>
-            <audio controls>
-              <source src={t["url"]} type="audio/mpeg" />
-            </audio>
-            <div id="waveform" className="border-2 w-full text-white"></div>
-          </div>
-        ))}
+        <div>{handleLoadCards()}</div>
       </div>
     </div>
   );
