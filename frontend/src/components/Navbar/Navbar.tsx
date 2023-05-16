b0ec4d6899f9f24149eb5087f1c295b037d0f0fc
Jack Thacker <jackthacker89@gmail.com>
2023-05-16 16:58:56 +0100

    Nav link styles

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -18,22 +18,34 @@ const Navbar = () => {
     await router.push(`/users/${userID}`);
   };
 
+  // const handleGenerateHTML = () => {
+  //   if (token) {
+  //     // return nav with profile etc
+  //   } else {
+  //     // return nav with signin etc
+  //   }
+  // };
+
+  // Logo either main room or landing page depending on token
+
   return (
     <div className="flex flex-row items-center justify-between w-full h-24 px-12">
       <picture className="flex flex-col items-center justify-center h-full w-64">
         <source srcSet={logoDark.src} media="(prefers-color-scheme: dark)" />
         <Image src={logoLight} alt="My image" width={300} height={300} />
       </picture>
-      <button
-        className="py-2 px-4 hover:pointer-cursor"
-        onClick={routeToProfile}>
-        Profile
-      </button>
-      <button
-        className="py-2 px-4 hover:pointer-cursor"
-        onClick={removeUserDetails}>
-        Sign Out
-      </button>
+      <div className="flex flex-row item-center justify-between gap-12">
+        <button
+          className="py-2 px-4 hover:pointer-cursor font-bold"
+          onClick={routeToProfile}>
+          Profile
+        </button>
+        <button
+          className="py-2 px-4 hover:pointer-cursor font-bold"
+          onClick={removeUserDetails}>
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
