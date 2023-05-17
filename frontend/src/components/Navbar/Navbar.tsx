0cdab14198c98ea2ad8a0687db15184fc0661caa
Jack Thacker <jackthacker89@gmail.com>
2023-05-17 18:09:22 +0100

    Navbar links indicate user location (styled)

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -5,9 +5,11 @@ import Image from "next/image";
 import logoDark from "../../../public/brainbeats-logo-dark-md.png";
 import logoLight from "../../../public/brainbeats-logo-light-md.png";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
   const removeUserDetails = () => {
     window.localStorage.clear();
@@ -33,16 +35,47 @@ const Navbar = () => {
         <Image src={logoLight} alt="My image" width={300} height={300} />
       </picture>
       <div className="flex flex-row item-center justify-between gap-12">
-        <button
-          className="py-2 px-4 hover:pointer-cursor font-bold"
-          onClick={routeToProfile}>
-          Profile
-        </button>
-        <button
-          className="py-2 px-4 hover:pointer-cursor font-bold"
-          onClick={removeUserDetails}>
-          Sign Out
-        </button>
+        <div>
+          <Link
+            href={`/users/${window.localStorage.getItem("username")}`}
+            className="py-2 px-4 hover:pointer-cursor font-bold"
+            onClick={routeToProfile}>
+            Profile
+          </Link>
+          <div
+            className={
+              pathname.includes("/users/")
+                ? "h-2 rounded-md bg-gradient-to-r from-orange-600 to-pink-400"
+                : "h-2"
+            }></div>
+        </div>
+        <div>
+          <Link
+            href="/rooms"
+            className="py-2 px-4 hover:pointer-cursor font-bold">
+            Rooms
+          </Link>
+          <div
+            className={
+              pathname === "/rooms"
+                ? "h-2 rounded-md bg-gradient-to-r from-orange-600 to-pink-400"
+                : "h-2"
+            }></div>
+        </div>
+        <div>
+          <Link
+            href="/login"
+            className="py-2 px-4 hover:pointer-cursor font-bold"
+            onClick={removeUserDetails}>
+            Sign Out
+          </Link>
+          <div
+            className={
+              pathname === "/login"
+                ? "h-2 rounded-md bg-gradient-to-r from-orange-600 to-pink-400"
+                : "h-2"
+            }></div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/NotAuthNavbar/NotAuthNavbar.tsx
+++ frontend/src/components/NotAuthNavbar/NotAuthNavbar.tsx
@@ -4,11 +4,12 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import logoDark from "../../../public/brainbeats-logo-dark-md.png";
 import logoLight from "../../../public/brainbeats-logo-light-md.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NotAuthNavbar = () => {
   const router = useRouter();
-
-  // Logo either main room or landing page depending on token
+  const pathname = usePathname();
 
   return (
     <div className="flex flex-row items-center justify-between w-full h-24 px-12">
@@ -20,6 +21,34 @@ const NotAuthNavbar = () => {
         <source srcSet={logoDark.src} media="(prefers-color-scheme: dark)" />
         <Image src={logoLight} alt="My image" width={300} height={300} />
       </picture>
+      <div className="flex flex-row item-center justify-between gap-12">
+        <div>
+          <Link
+            href="/signup"
+            className="py-2 px-4 hover:pointer-cursor font-bold">
+            Sign Up
+          </Link>
+          <div
+            className={
+              pathname === "/signup"
+                ? "h-2 rounded-md bg-gradient-to-r from-orange-600 to-pink-400"
+                : "h-2"
+            }></div>
+        </div>
+        <div>
+          <Link
+            href="/login"
+            className="py-2 px-4 hover:pointer-cursor font-bold">
+            Sign In
+          </Link>
+          <div
+            className={
+              pathname === "/login"
+                ? "h-2 rounded-md bg-gradient-to-r from-orange-600 to-pink-400"
+                : "h-2"
+            }></div>
+        </div>
+      </div>
     </div>
   );
 };
